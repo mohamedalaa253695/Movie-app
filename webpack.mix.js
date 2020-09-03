a4416c9e6d652acfdc963837eaf8ea9618478df3
mohamedalaa253695 <mohamedalaa253695@gmail.com>
2020-09-03 03:06:57 +0200

    cleaned up and minified  css fils

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +12,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+
 mix.postCss('resources/css/main.css', 'public/css', [
     require('tailwindcss'),
-  ]);
+  ])
+  .purgeCss();
